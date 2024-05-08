2998d01fe2762cf9395c7f8708f05473e4eb329a
Andrey Luiz <andrey.luiz@sonarsource.com>
2024-05-08 15:38:20 +0200

    Setup lcov

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 /** @type {import('jest').Config} */
 const config = {
   transform: {
-    '\\.[jt]sx?$': 'babel-jest'
+    "\\.[jt]sx?$": "babel-jest",
   },
-  transformIgnorePatterns: ['node_modules']
+  transformIgnorePatterns: ["node_modules"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
 };
-  
+
 module.exports = config;
